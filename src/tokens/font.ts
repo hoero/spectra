73ad2684577510e909338426ccb456308f90f0ba
Hoeru <hoeru@outlook.com>
2023-09-08 18:52:04 -0400

    Token update: font size

--- src/tokens/font.ts
+++ src/tokens/font.ts
@@ -2,51 +2,56 @@ import { Font, Responsive, Data, Element, Rem } from 'espectro';
 
 const { breakpoints } = Responsive,
     { spacing } = Element,
-    { size } = Font,
     { rpts, unitless, scaled } = Rem;
 
 const headings: Data.Attribute[] = [Font.bold];
 
-const font = {
-    size: {
-        base: scaled(1),
-        scaled2x: scaled(2),
-        scaled3x: scaled(3),
-        scaled4x: scaled(4),
-    },
+const size = {
+    base: scaled(1),
+    scaled2x: scaled(2),
+    scaled3x: scaled(3),
+    scaled4x: scaled(4),
+    scaled5x: scaled(5),
+    scaled6x: scaled(6),
 };
 
 function h1(device: Responsive.Device): Data.Attribute[] {
     return [
-        size(rpts(device, { ...breakpoints, default: 2.4, phone: 2.8 })),
+        Font.size(rpts(device, { ...breakpoints, default: 2.4, phone: 2.8 })),
         spacing(unitless(2.4, 1.165)),
     ];
 }
 
 function h2(device: Responsive.Device): Data.Attribute[] {
     return [
-        size(rpts(device, { ...breakpoints, default: 2.1, phone: 2.4 })),
+        Font.size(rpts(device, { ...breakpoints, default: 2.1, phone: 2.4 })),
         spacing(unitless(2.1, 1.195)),
     ];
 }
 
 function h3(device: Responsive.Device): Data.Attribute[] {
     return [
-        size(rpts(device, { ...breakpoints, default: 1.8, phone: 2.1 })),
+        Font.size(rpts(device, { ...breakpoints, default: 1.8, phone: 2.1 })),
         spacing(unitless(1.8, 1.25)),
     ];
 }
 
 function h4(device: Responsive.Device): Data.Attribute[] {
-    return [size(rpts(device, { ...breakpoints, default: 1.4, phone: 1.6 }))];
+    return [
+        Font.size(rpts(device, { ...breakpoints, default: 1.4, phone: 1.6 })),
+    ];
 }
 
 function h5(device: Responsive.Device): Data.Attribute[] {
-    return [size(rpts(device, { ...breakpoints, default: 1.2, phone: 1.6 }))];
+    return [
+        Font.size(rpts(device, { ...breakpoints, default: 1.2, phone: 1.6 })),
+    ];
 }
 
 function h6(device: Responsive.Device): Data.Attribute[] {
-    return [size(rpts(device, { ...breakpoints, default: 0.8, phone: 1 }))];
+    return [
+        Font.size(rpts(device, { ...breakpoints, default: 0.8, phone: 1 })),
+    ];
 }
 
-export { h1, h2, h3, h4, h5, h6, headings, font };
+export { h1, h2, h3, h4, h5, h6, headings, size };
